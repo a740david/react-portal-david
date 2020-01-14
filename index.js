dd81c2c7480cca46ea551da1188cb1b32158a434
a740david <54502632+a740david@users.noreply.github.com>
2020-01-14 15:43:21 +0200

    add tages

---
+++ index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { Container, Header, List } from "semantic-ui-react";
+
+import Example from "./App";
+
+const App = ({ children }) => (
+  <Container style={{ margin: 20 }}>
+    <List bulleted>
+      <List.Item
+        
+      />
+    </List>
+    {children}
+  </Container>
+);
+
+// TODO: Switch to https://github.com/palmerhq/the-platform#stylesheet when it will be stable
+const styleLink = document.createElement("link");
+styleLink.rel = "stylesheet";
+styleLink.href = "https://cdn.jsdelivr.net/npm/semantic-ui/dist/semantic.min.css";
+document.head.appendChild(styleLink);
+
+
+
+ReactDOM.render(
+  <App>
+    <Example />
+  </App>,
+  document.getElementById("root")
+);
+
+
